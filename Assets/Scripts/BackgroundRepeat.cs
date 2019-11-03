e5b2fce8be4335eef68c2f1f6c958c61316046fc
Mattias <osth.mattias@gmail.com>
2019-11-03 20:38:57 +0900

    project structure /w parallax and animated char

---
+++ Assets/Scripts/BackgroundRepeat.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRepeat : MonoBehaviour
+{
+    private float imageWidth;
+    private Rigidbody2D body;
+    private Vector2 posVector = new Vector2();
+    public float speedEffect;
+
+    void Start()
+    {
+        imageWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+        body = GetComponent<Rigidbody2D>();
+        body.velocity = new Vector2(speedEffect, 0);
+        posVector.x = body.transform.position.x;
+        posVector.y = body.transform.position.y;
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (body.transform.position.x <= -imageWidth)
+        {
+            posVector.x = transform.position.x + imageWidth * 2f;
+            body.transform.position = posVector;
+        }
+
+        
+    }
+}
