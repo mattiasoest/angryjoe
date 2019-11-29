805b67d86df8273820c3700928ab77cd51f52694
Mattias <osth.mattias@gmail.com>
2019-11-30 00:46:53 +0900

    touch control

--- Assets/Scripts/core/Player.cs
+++ Assets/Scripts/core/Player.cs
@@ -15,8 +15,6 @@ public class Player : MonoBehaviour {
     public float speed;
     public Transform feetCollider;
     public float checkRaduis;
-    public float jumpForce;
-    public float downForce;
     public LayerMask whatIsGround;
     public float jumpTime;
     public BoxCollider2D upperCollider;
@@ -29,6 +27,11 @@ public class Player : MonoBehaviour {
     private float counter;
     private bool isDoubleJumpPlaying;
     private float scoreTimer = 0.9f;
+    private float jumpYScreenPos = -2.9f;
+    private float jumpForce = 10f;
+    private float downForce = 100f;
+    private float jumpForceTouch = 8f;
+    private float downForceTouch = 80f;
 
     public void ResetJumpTrigger() {
         isDoubleJumpPlaying = false;
@@ -44,63 +47,9 @@ public class Player : MonoBehaviour {
     void Update() {
         isGrounded = Physics2D.OverlapCircle(feetCollider.position, 0.1f, whatIsGround);
         if (isAlive && StageController.instance.currentState == GAME_STATE.GAMEPLAY) {
-
-            if ((jumps > 0 || isGrounded) && Input.GetKeyDown(KeyCode.Space)) {
-
-                ResetSliding();
-                if (isGrounded) {
-                    jumps = DEFAULT_JUMPS;
-                }
-                if (jumps < DEFAULT_JUMPS) {
-                    isDoubleJumpPlaying = true;
-                    animator.SetBool("shouldPlayDoubleJump", isDoubleJumpPlaying);
-                    animator.SetTrigger("doubleJump");
-
-                }
-                jumps--;
-                rb.velocity = Vector2.up * jumpForce;
-                counter = jumpTime;
-                isJumping = true;
-
-                //AudioManager.instance.Play("jump2");
-                AudioManager.instance.PlayJump();
-            }
-
-            if (Input.GetKey(KeyCode.Space) && isJumping) {
-                if (counter > 0f) {
-                    rb.velocity = Vector2.up * jumpForce;
-                    counter -= Time.deltaTime;
-                } else {
-                    isJumping = false;
-                }
-            } else if (Input.GetKey(KeyCode.DownArrow) && !isGrounded) {
-                rb.velocity += Vector2.down * downForce * Time.deltaTime;
-                if (rb.velocity.y <= -30) {
-                    velVector.y = -30;
-                    rb.velocity = velVector;
-                }
-
-            } else if (Input.GetKey(KeyCode.DownArrow) && isGrounded && !isSliding) {
-                //AudioManager.instance.PlaySlide();
-                isSliding = true;
-                animator.SetBool("isSliding", true);
-                upperCollider.enabled = false;
-            }
-
-            if (Input.GetKeyDown(KeyCode.DownArrow)) {
-                AudioManager.instance.PlayDownForce();
-            }
-
-            if (Input.GetKeyUp(KeyCode.Space)) {
-                isJumping = false;
-            }
-
-            if (Input.GetKeyUp(KeyCode.DownArrow)) {
-                ResetSliding();
-            }
-
-            scoreTimer -= Time.deltaTime;
+            TouchInput();
         }
+        scoreTimer -= Time.deltaTime;
         animator.SetBool("isGrounded", isGrounded);
     }
 
@@ -119,6 +68,117 @@ public class Player : MonoBehaviour {
                 scoreTimer = 0.9f;
                 StageController.instance.UpdateScore();
             }
+        }
+    }
+
+    private void TouchInput() {
+        Vector3 pointerPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log(pointerPos.y);
+        if ((jumps > 0 || isGrounded) && Input.GetMouseButtonDown(0) && pointerPos.y > jumpYScreenPos) {
+            ResetSliding();
+            if (isGrounded) {
+                jumps = DEFAULT_JUMPS;
+            }
+            if (jumps < DEFAULT_JUMPS) {
+                isDoubleJumpPlaying = true;
+                animator.SetBool("shouldPlayDoubleJump", isDoubleJumpPlaying);
+                animator.SetTrigger("doubleJump");
+
+            }
+            jumps--;
+            rb.velocity = Vector2.up * jumpForceTouch;
+            counter = jumpTime;
+            isJumping = true;
+
+            //AudioManager.instance.Play("jump2");
+            AudioManager.instance.PlayJump();
+        }
+
+        if (Input.GetMouseButton(0) && pointerPos.y > jumpYScreenPos && isJumping) {
+            if (counter > 0f) {
+                rb.velocity = Vector2.up * jumpForceTouch;
+                counter -= Time.deltaTime;
+            } else {
+                isJumping = false;
+            }
+        } else if (Input.GetMouseButton(0) && pointerPos.y <= jumpYScreenPos && !isGrounded) {
+            rb.velocity += Vector2.down * downForceTouch * Time.deltaTime;
+            if (rb.velocity.y <= -30) {
+                velVector.y = -30;
+                rb.velocity = velVector;
+            }
+
+        } else if (Input.GetMouseButton(0) && pointerPos.y <= jumpYScreenPos && isGrounded && !isSliding) {
+            //AudioManager.instance.PlaySlide();
+            isSliding = true;
+            animator.SetBool("isSliding", true);
+            upperCollider.enabled = false;
+        }
+
+        if (Input.GetMouseButtonDown(0) && pointerPos.y <= jumpYScreenPos) {
+            AudioManager.instance.PlayDownForce();
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            isJumping = false;
+            ResetSliding();
+        }
+
+    }
+
+    private void KeyBoardInput() {
+        if ((jumps > 0 || isGrounded) && Input.GetKeyDown(KeyCode.Space)) {
+
+            ResetSliding();
+            if (isGrounded) {
+                jumps = DEFAULT_JUMPS;
+            }
+            if (jumps < DEFAULT_JUMPS) {
+                isDoubleJumpPlaying = true;
+                animator.SetBool("shouldPlayDoubleJump", isDoubleJumpPlaying);
+                animator.SetTrigger("doubleJump");
+
+            }
+            jumps--;
+            rb.velocity = Vector2.up * jumpForce;
+            counter = jumpTime;
+            isJumping = true;
+
+            //AudioManager.instance.Play("jump2");
+            AudioManager.instance.PlayJump();
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping) {
+            if (counter > 0f) {
+                rb.velocity = Vector2.up * jumpForce;
+                counter -= Time.deltaTime;
+            } else {
+                isJumping = false;
+            }
+        } else if (Input.GetKey(KeyCode.DownArrow) && !isGrounded) {
+            rb.velocity += Vector2.down * downForce * Time.deltaTime;
+            if (rb.velocity.y <= -30) {
+                velVector.y = -30;
+                rb.velocity = velVector;
+            }
+
+        } else if (Input.GetKey(KeyCode.DownArrow) && isGrounded && !isSliding) {
+            //AudioManager.instance.PlaySlide();
+            isSliding = true;
+            animator.SetBool("isSliding", true);
+            upperCollider.enabled = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            AudioManager.instance.PlayDownForce();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            isJumping = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow)) {
+            ResetSliding();
         }
     }
 
