1d48f0d0d49c0b6b736f87dd84a02984a3b7941a
Mattias <osth.mattias@gmail.com>
2019-12-01 21:59:22 +0900

    improved playfab login fetching username

--- Assets/Scripts/manager/PlayfabManager.cs
+++ Assets/Scripts/manager/PlayfabManager.cs
@@ -2,27 +2,38 @@
 using PlayFab.ClientModels;
 using PlayFab.Json;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayfabManager : MonoBehaviour {
 
     public static PlayfabManager instance;
 
+    public Text nameText;
+
+    [HideInInspector]
+    public string playerName;
+
+    [HideInInspector]
+    public bool hasUsername;
+
     private const string SCORE_STATISTIC = "highscore";
+
+    private bool debug;
 
     public void Awake() {
         instance = this;
     }
 
     public void Start() {
-        if (string.IsNullOrEmpty(PlayFabSettings.TitleId)) {
+        #if UNITY_EDITOR
+                Debug.Log("Unity Editor");
+        debug = true;
+        #endif
+        if (string.IsNullOrWhiteSpace(PlayFabSettings.TitleId)) {
             PlayFabSettings.TitleId = "5B418";
         }
-        LoginWithCustomIDRequest request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
-        PlayFabClientAPI.LoginWithCustomID(request, result => {
-            Debug.Log("Logged in to PlayFab!");
-        }, error => {
-            Debug.LogError(error.GenerateErrorReport());
-        });
+
+        PlayfabLogin();
     }
 
     public void SetDisplayName(string name) {
@@ -63,5 +74,80 @@ public class PlayfabManager : MonoBehaviour {
         }, error => {
             Debug.Log(error.GenerateErrorReport());
         });
+    }
+
+    private void PlayfabLogin() {
+        if (!debug) {
+            #if UNITY_ANDROID
+                LoginWithAndroidDeviceIDRequest requestAndroid = new LoginWithAndroidDeviceIDRequest
+                { AndroidDeviceId = GetMobileId(),
+                    CreateAccount = true,
+                    InfoRequestParameters = new GetPlayerCombinedInfoRequestParams() {
+                        GetPlayerProfile = true,
+                        ProfileConstraints = new PlayerProfileViewConstraints() {
+                            ShowDisplayName = true,
+                        }
+                    }
+                };
+                PlayFabClientAPI.LoginWithAndroidDeviceID(requestAndroid, OnLoginMobileSuccess, OnLoginMobileFailure);
+            #endif
+            #if UNITY_IOS
+            LoginWithIOSDeviceIDRequest requestIOS = new LoginWithIOSDeviceIDRequest {
+            DeviceId = ReturnMobileID(),
+            CreateAccount = true,
+            InfoRequestParameters = new GetPlayerCombinedInfoRequestParams() {
+                GetPlayerProfile = true,
+                ProfileConstraints = new PlayerProfileViewConstraints() {
+                    ShowDisplayName = true,
+                }
+            }
+            };
+            PlayFabClientAPI.LoginWithIOSDeviceID(requestIOS, OnLoginMobileSuccess, OnLoginMobileFailure);
+        #endif
+        } else {
+            LoginWithCustomIDRequest request = new LoginWithCustomIDRequest {
+                CustomId = "GettingStartedGuide",
+                CreateAccount = true,
+                InfoRequestParameters = new GetPlayerCombinedInfoRequestParams() {
+                    GetPlayerProfile = true,
+                    ProfileConstraints = new PlayerProfileViewConstraints() {
+                        ShowDisplayName = true,
+                    }
+                }
+            };
+            PlayFabClientAPI.LoginWithCustomID(request, result => {
+                string username = result.InfoResultPayload.PlayerProfile.DisplayName;
+                if (string.IsNullOrEmpty(username)) {
+                    hasUsername = false;
+                } else {
+                    hasUsername = true;
+                    playerName = username;
+                    nameText.enabled = true;
+                    nameText.text = $"Logged in as: {playerName}";
+                }
+            }, error => {
+                Debug.LogError(error.GenerateErrorReport());
+            });
+        }
+    }
+
+    private void OnLoginMobileSuccess(LoginResult result) {
+        string username = result.InfoResultPayload.PlayerProfile.DisplayName;
+        if (string.IsNullOrEmpty(username)) {
+            hasUsername = false;
+        } else {
+            hasUsername = true;
+            playerName = username;
+            nameText.enabled = true;
+            nameText.text = $"Logged in as: {playerName}";
+        }
+    }
+
+    private void OnLoginMobileFailure(PlayFabError error) {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+
+    private string GetMobileId() {
+        return SystemInfo.deviceUniqueIdentifier;
     }
 }
