296e852cb3950c40de5a36a4c08107f6f58620ba
Mattias <osth.mattias@gmail.com>
2019-12-01 11:50:29 +0900

    playfab setup

---
+++ Assets/Scripts/manager/PlayfabManager.cs
@@ -0,0 +1,23 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+public class PlayfabManager : MonoBehaviour {
+    public void Start() {
+        if (string.IsNullOrEmpty(PlayFabSettings.TitleId)) {
+            PlayFabSettings.TitleId = "5B418";
+        }
+        var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
+        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+    }
+
+    private void OnLoginSuccess(LoginResult result) {
+        Debug.Log("Congratulations, you made your first successful API call!");
+    }
+
+    private void OnLoginFailure(PlayFabError error) {
+        Debug.LogWarning("Something went wrong with your first API call.  :(");
+        Debug.LogError("Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
+    }
+}
