a5c3d8a8a75fe2f714b21554c7e3d97c4351ae3f
Mattias <osth.mattias@gmail.com>
2019-12-02 17:09:58 +0900

    new leaderboard art, toggle debug code

--- Assets/Scripts/ui/LeaderboardUI.cs
+++ Assets/Scripts/ui/LeaderboardUI.cs
@@ -1,9 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using System;
 
 public class LeaderboardUI : MonoBehaviour {
 
+    private const string WEEKLY_NAME = "ToggleWeekly";
+    private const string GLOBAL_NAME = "ToggleGlobal";
+
+
+    public ToggleGroup toggleGroup;
+
+    public Toggle currentSelected {
+        get {
+            return toggleGroup.ActiveToggles().FirstOrDefault();
+        }
+    }
+
+    void Start() {
+
+    }
 
     // Update is called once per frame
     void Update() {
@@ -12,5 +30,23 @@ public class LeaderboardUI : MonoBehaviour {
 
     public void CloseButton() {
         gameObject.SetActive(false);
+    }
+
+    public void SelectToggle(int id) {
+        Toggle[] toggles = toggleGroup.GetComponentsInChildren<Toggle>();
+        toggles[id].isOn = true;
+    }
+
+    public void Toggle() {
+        switch (currentSelected.name) {
+            case GLOBAL_NAME:
+                Debug.Log("global");
+                break;
+            case WEEKLY_NAME:
+                Debug.Log("weekly");
+                break;
+            default:
+                throw new Exception($"Invalid toogle name {currentSelected.name}");
+        }
     }
 }
