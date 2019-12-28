c2bfeec1c62404a0583959b54dfbc22eba375051
Mattias <osth.mattias@gmail.com>
2019-12-28 15:30:36 +0900

    Academy script

---
+++ Assets/Scripts/ai/BruceAcademy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using static StageController;
+
+public class BruceAcademy : Academy {
+
+    public override void InitializeAcademy() {
+        // Debug.Log("Academy INIT");
+        // StageController.instance.FastStartGame();
+    }
+
+    public override void AcademyReset() {
+        Debug.Log("Academy reset");
+        StageController.instance.ResetArea();
+    }
+}
